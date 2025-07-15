2e7e88244ab0a04a0af322400624ad227cdbbe47
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-07-15 13:40:36 +0500

    Create for loop to populate library onto DOM

--- script.js
+++ script.js
@@ -19,5 +19,37 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 addBookToLibrary("Harry Potter", "J.K. Rowling", 420, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 543, true);
+addBookToLibrary("12th Night", "William Shakespeare", 221, false);
 console.log(myLibrary);
-console.log(crypto.randomUUID())
+
+const container = document.querySelector("#container");
+
+myLibrary.forEach(book => {
+  const card = document.createElement("div");  
+  card.classList.add("card");
+
+  const title = document.createElement("h3");
+  title.classList.add("title");
+  title.textContent = book.title;
+
+  const author = document.createElement("p");
+  author.classList.add("author");
+  author.textContent = book.author;
+
+  const pages = document.createElement("p");
+  pages.classList.add("pages");
+  pages.textContent = book.pages;
+
+  const isRead = document.createElement("p");
+  isRead.classList.add("isRead");
+  isRead.textContent = book.isRead;
+
+  container.appendChild(card);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(isRead);
+
+
+});
